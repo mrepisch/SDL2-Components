268ce6f52db73bf88e1c6490d767d15bfdc7c3db
sascha <mr.episch@hotmail.com>
2019-01-26 20:30:42 +0100

    new file:   Basic16_16Tileset.tsx 	new file:   groundTileset.tsx 	new file:   ground_tileset.png 	new file:   test.bmp 	modified:   testmap.tmx 	new file:   testmap.xml

---
+++ Basic16_16Tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Basic16_16Tileset" tilewidth="16" tileheight="16" tilecount="12" columns="4">
+ <image source="../2sAssets/Basic16_16Tileset.png" width="64" height="48"/>
+ <tile id="5">
+  <properties>
+   <property name="tileAttr" value="test"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ groundTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="ground_tileset" tilewidth="32" tileheight="32" tilecount="24" columns="4">
+ <image source="ground_tileset.png" width="128" height="192"/>
+</tileset>
